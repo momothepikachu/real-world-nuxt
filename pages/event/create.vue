b26cf9f1ca38d2259562c99cd3700d462709ae93
Monica Rao <monicaxrao@gmail.com>
2019-07-05 14:54:03 -0400

    remove ic components in create.vue

--- pages/event/create.vue
+++ pages/event/create.vue
@@ -1,27 +1,9 @@
 <template>
   <div>
     <h1>Create an event</h1>
-    <ElInstance
-      v-model="outJsonString"
-      input-type="dragNDrop"
-      complexity="simple"
-    />
   </div>
 </template>
 <script>
-import Vue from 'vue'
-import IcInputs from '@explorelearning/ic-inputs'
-import '@explorelearning/ic-inputs/dist/ic-inputs.css'
-import draggable from 'vuedraggable'
-import IcFormBuilder from '@explorelearning/ic-form-builder'
-import '@explorelearning/ic-form-builder/dist/ic-form-builder.css'
-import 'bootstrap'
-import 'bootstrap/dist/css/bootstrap.min.css'
-
-Vue.use(IcFormBuilder)
-Vue.use(IcInputs)
-Vue.component('draggable', draggable)
-
 export default {
   head() {
     return {
@@ -33,11 +15,6 @@ export default {
           content: 'You can create a new event in your neighborhood'
         }
       ]
-    }
-  },
-  data() {
-    return {
-      outJsonString: ''
     }
   }
 }
