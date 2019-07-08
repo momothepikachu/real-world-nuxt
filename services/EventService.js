0f53992bbfeed38af10ac1dee7a2115c57255800
Monica Rao <monicaxrao@gmail.com>
2019-07-08 09:37:29 -0400

    delete slash at the end of url

--- services/EventService.js
+++ services/EventService.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const apiClient = axios.create({
-  baseURL: `https://my-json-server.typicode.com/Code-Pop/real-world-nuxt`,
+  baseURL: `http://my-json-server.typicode.com/momothepikachu/real-world-nuxt`,
   withCredentials: false,
   headers: {
     Accept: 'application/json',
